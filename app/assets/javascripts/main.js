2b7856ee7500d2114fc954ead70657ea397b2602
trowbridge.james@gmail.com <trowbridge.james@gmail.com>
2017-02-16 14:25:12 -0500

    change interval

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
     resizeToMax(image);
   });
   //CHANGE NUMBER BELOW FOR IMAGE SCROLLING INTERVAL
-  $('.carousel').carousel({interval: 1000, wrap: true, pause: null});
+  $('.carousel').carousel({interval: 30000, wrap: true, pause: null});
 
   //CHANGE NUMBER BELOW FOR PAGE REFRESH INTERVAL
   var total_items = $('.item').size();
-  setInterval(function() { window.location.reload(); }, (total_items * 1000) ); 
+  setInterval(function() { window.location.reload(); }, (total_items * 30000) ); 
 });
 
 function resizeToMax(id){
